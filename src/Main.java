bad7fec10d007d0a690f3fb9c07a2f9c62e59842
Eduardo433781 <eduardo.coerse@gmail.com>
2025-03-29 10:36:23 -0300

    feat: aa

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,105 @@
 import javax.swing.*;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
 
-        String InputSenaiJava = JOptionPane.showInputDialog("??");
-        int input = Integer.parseInt(InputSenaiJava);
-        input++;
-        
+       // String InputSenaiJava = JOptionPane.showInputDialog("??");
+       // int input = Integer.parseInt(InputSenaiJava);
+       // input++;
+
+
+//        String[] SenaiValues = {"Aluno 1", "Aluno 2", "Aluno 3"};
+//        String Input2 = (String)JOptionPane.showInputDialog(
+//                null,
+//
+//                "Titulo da caixa de diálogo",
+//                "Esta é a mensagem",
+//                1,
+//                null,
+//                SenaiValues,
+//                SenaiValues[0]
+//        );
+
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println();
+//        int x = scanner.nextInt(1);
+//        double y = scanner.nextDouble();
+//        String z = scanner.next("Senai");
+//        System.out.println(scanner);
+//        scanner.close();
+
+//        Scanner sc = new Scanner(
+//                Input04.class.getResourceAsStream("fakeFile.txt"));
+//        sc.close();
+//
+//
+//                        String nome = JOptionPane.showInputDialog("Digite seu nome:");
+//                JOptionPane.showMessageDialog(null, "Olá, " + nome + "!");
+//
+
+//        String input = JOptionPane.showInputDialog("Digite um número:");
+//        int numero = Integer.parseInt(input);
+//        JOptionPane.showMessageDialog(null, "O número digitado + 1 é: " + (numero + 1));
+
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Digite seu nome: ");
+//        String nome = scanner.nextLine();
+//
+//        System.out.println("Olá, " + nome + "!");
+//
+//        scanner.close();
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Digite um número inteiro: ");
+//        int numero = scanner.nextInt();
+//
+//        System.out.println("O número digitado + 1 é: " + (numero + 1));
+//
+//        scanner.close();
+
+//        try {
+//            File arquivo = new File("dados.txt");
+//            Scanner scanner = new Scanner(arquivo);
+//
+//            while (scanner.hasNextLine()) {
+//                String linha = scanner.nextLine();
+//                System.out.println(linha);
+//            }
+//
+//            scanner.close();
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Arquivo não encontrado.");
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
